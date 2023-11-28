14963604474774958692e3993939caad251cea85
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-28 12:57:02 +0100

    feat(backend): create report model

---
+++ backend/src/models/Report.js
@@ -0,0 +1,12 @@
+/**
+ * This model describes how a report object presented, which belongs to an employee
+ */
+class Report {
+  constructor(period, opinionSum, remarks) {
+    this.period = period;
+    this.opinionSum = opinionSum;
+    this.remarks = remarks;
+  }
+}
+
+module.exports = Report
