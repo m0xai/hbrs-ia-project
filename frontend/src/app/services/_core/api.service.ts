eda93e201a074a18468dbebcd8eb21cf732d3157
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-12-01 22:25:46 +0100

    feat(frontend): create api service for sending basic API requests

---
+++ frontend/src/app/services/_core/api.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { BaseModel } from '../../models/_core/BaseModel';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiService<T extends BaseModel<T>> {
+    public apiURL = '';
+    public apiBase = 'http://127.0.0.1:8080/api/';
+
+    constructor(private httpClient: HttpClient, public tConstructor: { new(m: Partial<T>, ...args: unknown[]): T },
+                private apiPath: string
+    ) {
+        this.apiURL = this.apiBase + apiPath;
+    }
+
+
+    public create(resource: Partial<T> & { toJson: () => T }): Observable<T> {
+        return this.httpClient
+            .post<T>(`${this.apiURL}`, resource.toJson())
+            .pipe(map((result) => new this.tConstructor(result)));
+    }
+
+    public list(): Observable<T[]> {
+        return this.httpClient
+            .get<T[]>(this.apiURL)
+            .pipe(map((result) => result.map((i) => new this.tConstructor(i))));
+    }
+
+    public get(id: number): Observable<T> {
+        return this.httpClient
+            .get<T>(`${this.apiURL}${id}/`)
+            .pipe(map((result) => new this.tConstructor(result)));
+    }
+
+    public update(resource: Partial<T> & { toJson: () => T }): Observable<T> {
+        return this.httpClient
+            .put<T>(`${this.apiURL}${resource.id}/`, resource.toJson())
+            .pipe(map((result) => new this.tConstructor(result)));
+    }
+
+    public partialUpdate(resource: Partial<T> & { toJson: () => T }): Observable<T> {
+        return this.httpClient
+            .patch<T>(`${this.apiURL}${resource.id}/`, resource.toJson())
+            .pipe(map((result) => new this.tConstructor(result)));
+    }
+
+    public delete(id: number): Observable<T> {
+        return this.httpClient.delete<T>(`${this.apiURL}${id}/`);
+    }
+}
