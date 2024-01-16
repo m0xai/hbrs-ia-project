0ddae092f96a8b26dcab579d8169d5556e441e69
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-01-16 22:53:42 +0100

    feat(frontend): create review service for employee reports

---
+++ frontend/src/app/services/review/review.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from "@angular/core";
+import {ApiService} from "../_core/api.service";
+import {Review} from "../../models/Review";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ReviewService extends ApiService<Review> {
+  constructor(private http: HttpClient) {
+    super(http, Review, "reviews/");
+  }
+}
