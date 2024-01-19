1dc6b7748afc1ef6980537fac2dc29406e316404
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-01-19 23:37:22 +0100

    feat(frontend): create bonus calculator page

---
+++ frontend/src/app/pages/bonus-calculator-page/bonus-calculator-page.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BonusCalculatorPageComponent } from './bonus-calculator-page.component';
+
+describe('BonusCalculatorPageComponent', () => {
+  let component: BonusCalculatorPageComponent;
+  let fixture: ComponentFixture<BonusCalculatorPageComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BonusCalculatorPageComponent]
+    });
+    fixture = TestBed.createComponent(BonusCalculatorPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/pages/bonus-calculator-page/bonus-calculator-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bonus-calculator-page',
+  templateUrl: './bonus-calculator-page.component.html',
+  styleUrls: ['./bonus-calculator-page.component.css']
+})
+export class BonusCalculatorPageComponent {
+
+}
