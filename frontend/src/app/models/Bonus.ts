d92ae0d5e1d56d253c4a45e90908281349e2c861
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-02-18 14:52:30 +0100

    feat(frontend): create bonus service for working with bonuses and salaries

---
+++ frontend/src/app/models/Bonus.ts
@@ -0,0 +1,20 @@
+/**
+ * this model specifies the format to exchange a Bonus with the backend
+ */
+export class Bonus {
+  constructor(
+    public value: number | string,
+    public year: number | string,
+    public sid?: number | string,
+    private _remark?: string,
+    public verified?: boolean,
+  ) {}
+
+  get remark(): string {
+    return this._remark;
+  }
+
+  set remark(value: string) {
+    this._remark = value;
+  }
+}
