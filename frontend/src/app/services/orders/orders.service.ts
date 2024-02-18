2a3e1710a6d23374e2e9b9daebdd29b32941b7b9
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-02-18 14:48:50 +0100

    feat(frontend): create order service for working with salesmen, customers and orders

---
+++ frontend/src/app/services/orders/orders.service.ts
@@ -0,0 +1,69 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../../../environments/environment";
+
+@Injectable({
+  providedIn: "root",
+})
+export class OpenCRXService {
+  constructor(private http: HttpClient) {}
+
+  listAccounts(): Observable<any[]> {
+    return this.http.get<any[]>(environment.apiEndpoint + "/api/orders/accounts", {
+      withCredentials: true,
+    });
+  }
+
+  listCustomers(): Observable<any[]> {
+    return this.http.get<any[]>(environment.apiEndpoint + `/api/orders/accounts/customer`, {
+      withCredentials: true,
+    });
+  }
+
+  listSalesmen(): Observable<any[]> {
+    return this.http.get<any[]>(environment.apiEndpoint + `/api/orders/accounts/salesman`, {
+      withCredentials: true,
+    });
+  }
+
+  getCustomerAccount(uid: string | number): Observable<any[]> {
+    return this.http.get<any[]>(environment.apiEndpoint + `/api/orders/accounts/customer/${uid}`, {
+      withCredentials: true,
+    });
+  }
+
+  getSalesmanAccount(uid: string | number): Observable<any[]> {
+    return this.http.get<any[]>(environment.apiEndpoint + `/api/orders/accounts/salesman/${uid}`, {
+      withCredentials: true,
+    });
+  }
+
+  getSalesmanContracts(uid: string | number): Observable<object> {
+    return this.http.get<object>(
+      environment.apiEndpoint + `/api/orders/accounts/salesman/${uid}/contracts`,
+      {withCredentials: true},
+    );
+  }
+
+  listProducts(): Observable<object> {
+    return this.http.get<object>(environment.apiEndpoint + `/api/orders/products`, {
+      withCredentials: true,
+    });
+  }
+
+  getSalesOrderData(contractID: string | undefined): Observable<object> {
+    if (!contractID) return;
+
+    return this.http.get<object>(environment.apiEndpoint + `/api/orders/salesOrder/${contractID}`, {
+      withCredentials: true,
+    });
+  }
+
+  getSalesOrderPosition(contractID: string): Observable<object> {
+    return this.http.get<object>(
+      environment.apiEndpoint + `/api/orders/salesOrder/${contractID}/position`,
+      {withCredentials: true},
+    );
+  }
+}
